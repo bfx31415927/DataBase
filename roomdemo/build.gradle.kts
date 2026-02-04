762b7ea6751ee51bea25515ad3c367e5ad848074
smirnov_aa <smirnov_aa@vector.ru>
2026-02-05 00:29:31 +0300

    Добавил модуль RoomDemo, подправил системные файлы, не успел разобраться с searchResults, которая может быть null

---
+++ roomdemo/build.gradle.kts
@@ -0,0 +1,74 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.devtoolsKsp)
+    alias(libs.plugins.androidx.room)
+}
+
+android {
+    namespace = "com.example.roomdemo"
+    compileSdk {
+        version = release(36)
+    }
+
+    defaultConfig {
+        applicationId = "com.example.viewmodeldemo"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
+// Добавляем конфигурацию для Room
+    room {
+        schemaDirectory("$projectDir/schemas")
+    }
+
+    buildFeatures {
+        compose = true
+    }
+}
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose) // ← ЭТО НОВАЯ СТРОКА
+    implementation(libs.androidx.activity.compose) // ← ЭТО НОВАЯ СТРОКА
+    implementation(libs.androidx.room.runtime)//*
+    implementation(libs.androidx.room.ktx)//*
+    implementation(libs.androidx.compose.runtime.livedata)//*
+    annotationProcessor(libs.androidx.room.room.compiler)//*
+    ksp(libs.androidx.room.room.compiler)//*
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+}
